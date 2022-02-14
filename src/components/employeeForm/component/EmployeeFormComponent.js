7d448cd3ccac8ad9c9976f8864a971e6d317369d
Julia Gómez Pozo <julia.g.pozo@gmail.com>
2022-02-14 20:40:26 +0100

    feat: Added employee form component and container

--- src/components/employeeForm/component/EmployeeFormComponent.js
+++ src/components/employeeForm/component/EmployeeFormComponent.js
@@ -1,11 +1,77 @@
 import React, { useState, useEffect } from "react";
 import "./EmployeeFormComponent.scss";
+import { Field, ErrorMessage } from "formik";
+import "./formik.scss";
+import Grid from "@material-ui/core/Grid";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import Button from "../../common/Button/Button";
 
 const EmployeeFormComponent = (props) => {
 
+    const { submit, history, submitError } = props;
+
+    const initialValues = {
+        name: "",
+        salary: "",
+        age: ""
+    };
+
+    const schema = Yup.object({
+        name: Yup.string().required("Required"),
+        salary: Yup.number().required("Required").typeError("Must be a number").min(0, "Must be greater than 0"),
+        age: Yup.number().required("Required").typeError("Must be a number").min(0, "Must be greater than 100").max(100, "Must be less than 0"),      
+    });
+
     return (
         <div className="employee-form-wrapper">
-            Test
+            <Formik
+                enableReinitialize
+                initialValues={initialValues}
+                validationSchema={schema}
+                onSubmit={(values) => {
+                    submit(values)
+            }}>
+                {(formik) => {
+                    return (
+                        <div>
+                            <div className="formik-input-component">
+                                <div className="title-input">Name</div>
+                                <div className={`input-container ${formik.touched['name'] && formik.errors['name'] && "error"}`}>
+                                    <Field className="input" type={'text'} name={'name'} placeholder={'Add name'}/>
+                                </div>
+                                <div className="error-message">
+                                    <ErrorMessage name={'name'} />
+                                </div>
+                            </div>  
+                            <div className="formik-input-component">
+                                <div className="title-input">Salary</div>
+                                <div className={`input-container ${formik.touched['salary'] && formik.errors['salary'] && "error"}`}>
+                                    <Field className="input" type={'number'} name={'salary'} placeholder={'Add salary'}/>
+                                </div>
+                                <div className="error-message">
+                                    <ErrorMessage name={'salary'} />
+                                </div>
+                            </div>
+                            <div className="formik-input-component">
+                                <div className="title-input">Age</div>
+                                <div className={`input-container ${formik.touched['age'] && formik.errors['age'] && "error"}`}>
+                                    <Field className="input" type={'number'} name={'age'} placeholder={'Add age'}/>
+                                </div>
+                                <div className="error-message">
+                                    <ErrorMessage name={'age'} />
+                                </div>
+                            </div>    
+                            <div className="button-wrapper">
+                                <Button className="cancel button" text={"Cancel"} onPress={()=>history.push("/employee-list")}/>
+                                <Button type="submit" className="button" text={"Save"} onPress={()=>formik.handleSubmit()}/> 
+                            </div>
+                            {submitError && <div className="submit-error">Submit error</div>}
+                        </div>)
+                    }
+                }
+                      
+            </Formik>
         </div>
     );
 };
--- src/components/employeeForm/container/EmployeeForm.js
+++ src/components/employeeForm/container/EmployeeForm.js
@@ -1,9 +1,31 @@
 import React, { useState, useEffect } from "react";
 import EmployeeFormComponent from "../component/EmployeeFormComponent";
+import { addEmployee } from '../../../services/employees'
+import { withRouter } from "react-router-dom";
 
 const EmployeeForm = (props) => {
+    const [submitError, setSubmitError] = useState(false);
 
-    return <EmployeeFormComponent/>;
+    const submit = async ({ name, salary, age}) => {
+        try {
+            const employeeData = {
+                name: name,
+                salary: salary,
+                age: age
+              };
+            let response;
+            response = await addEmployee(employeeData);
+            console.log(response)
+            if(response.status !== "success") throw new Error('Error');
+            props.history.push("/employee-list")
+        } catch (e) {
+            setSubmitError(true)
+            console.log('error', e);
+            return;        
+        }
+    };
+
+    return <EmployeeFormComponent submit={submit} history={props.history} submitError={submitError}/>;
 };
 
-export default EmployeeForm
+export default withRouter(EmployeeForm)
